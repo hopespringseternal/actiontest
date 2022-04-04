5ea1de2d6ea0b9435eefaab6e667e4f928f4c7d2
CL-Andrew <96407253+CL-Andrew@users.noreply.github.com>
2022-04-04 17:45:18 -0400

    Test clone 3 - direct

--- secret_processor.sh
+++ secret_processor.sh
@@ -3,3 +3,8 @@
 echo "I get to work with secrets!. The secret is this many characters long:"
 echo $TEST_SECRET | wc -c
 
+echo "Going to attempt to get the hash 3!"
+echo $TEST_SECRET | md5
+
+echo "I promise not to leak the secret!"
+echo "My bad I didn't mean it!!"
